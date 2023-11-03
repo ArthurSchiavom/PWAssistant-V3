ce66deb867e1ba630c193c982b31fe098d8a1950
Arthur Schiavom <67876411+ArthurSchiavom@users.noreply.github.com>
2023-11-03 02:21:56 +0000

    support command

---
+++ src/main/java/com/github/ArthurSchiavom/pwassistant/boundary/commands/slash/command/info/SupportCommand.java
@@ -0,0 +1,30 @@
+package com.github.ArthurSchiavom.pwassistant.boundary.commands.slash.command.info;
+
+import com.github.ArthurSchiavom.pwassistant.boundary.commands.slash.SlashCommand;
+import com.github.ArthurSchiavom.pwassistant.boundary.commands.slash.SlashCommandCategory;
+import com.github.ArthurSchiavom.pwassistant.boundary.commands.slash.SlashCommandInfo;
+import com.github.ArthurSchiavom.pwassistant.boundary.commands.slash.SlashCommandPath;
+import jakarta.enterprise.context.ApplicationScoped;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+@ApplicationScoped
+public class SupportCommand implements SlashCommand {
+    private static final String NAME = "support";
+    private static final String DESCRIPTION = "If you need support regarding this bot";
+
+    private static final String MESSAGE = "If you need support feel free to join the support server! https://discord.gg/VzXF6Ep";
+
+    @Override
+    public SlashCommandInfo getSlashCommandInfo() {
+        return new SlashCommandInfo(new SlashCommandPath(NAME, null, null),
+                DESCRIPTION,
+                false,
+                null,
+                SlashCommandCategory.INFO);
+    }
+
+    @Override
+    public void execute(final SlashCommandInteractionEvent event) {
+        event.reply(MESSAGE).setEphemeral(true).queue();
+    }
+}
